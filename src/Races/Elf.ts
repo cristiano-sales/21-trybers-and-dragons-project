fa9bb4f248a2682a4ea999f1d257112a932bd9fb
Cristiano Sales <cristianosalesneves@gmail.com>
2022-07-27 18:21:38 -0300

    subclass elf

---
+++ src/Races/Elf.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+export default class Elf extends Race {
+  private _maxLifePoints: number;
+  private static _elfIntance = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._maxLifePoints = 99;
+
+    Elf._elfIntance += 1;
+  }
+
+  public get maxLifePoints(): number {
+    return this.maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    return Elf._elfIntance;
+  }
+}
